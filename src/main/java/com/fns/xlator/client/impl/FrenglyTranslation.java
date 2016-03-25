e8c7078dc6755a792fdca5dbcb0f900b32d017c4
Chris Phillipson <fastnsilver@gmail.com>
2016-03-25 13:11:20 -0700

    Get ready for deployment
    Beef up POM with new profiles for docker and Maven Site documentation.  Also, integrate with Travis-CI and Codecov.  Begin effort to generate REST API w/ Swagger.  Fix flaw in Locale validation.

---
+++ src/main/java/com/fns/xlator/client/impl/FrenglyTranslation.java
@@ -0,0 +1,43 @@
+package com.fns.xlator.client.impl;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder(value = { "src", "dest", "text", "translation" })
+public class FrenglyTranslation {
+
+    private String src;
+    private String dest;
+    private String text;
+    private String translation;
+
+    @JsonCreator
+    public FrenglyTranslation(@JsonProperty("src") String src, @JsonProperty("dest") String dest,
+            @JsonProperty("text") String text, @JsonProperty("translation") String translation) {
+        this.src = src;
+        this.dest = dest;
+        this.text = text;
+        this.translation = translation;
+    }
+
+
+    public String getSrc() {
+        return src;
+    }
+
+    public String getDest() {
+        return dest;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+}
