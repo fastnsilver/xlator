35c08b6213535ec13e3a69788f85c546aebd083c
Chris Phillipson <fastnsilver@gmail.com>
2016-03-29 21:52:52 -0700

    Prepare for deployment to AWS ECS

--- src/main/java/com/fns/xlator/App.java
+++ src/main/java/com/fns/xlator/App.java
@@ -10,18 +10,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration;
+import org.springframework.cloud.aws.cache.config.annotation.CacheClusterConfig;
+import org.springframework.cloud.aws.cache.config.annotation.EnableElastiCache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
-@EnableCaching
+import java.util.ArrayList;
+import java.util.List;
+
+// special thanks to https://github.com/spring-cloud-samples/aws-refapp/blob/master/src/main/java/org/springframework/cloud/aws/sample/ReferenceApplication.java
+
+@SpringBootApplication(exclude = ElastiCacheAutoConfiguration.class)
 public class App {
 
     @Configuration
     @ConditionalOnProperty(prefix = "app.defaults", name = "service", havingValue = "google")
-    static class GoogleApi {
+    static class GoogleApiConfig {
 
         @Bean
         public GoogleClientSettings googleClientSettings() {
@@ -38,7 +49,7 @@ public class App {
 
     @Configuration
     @ConditionalOnProperty(prefix = "app.defaults", name = "service", havingValue = "frengly")
-    static class FrenglyApi {
+    static class FrenglyApiConfig {
 
         @Bean
         public FrenglyClientSettings frenglyClientSettings() {
@@ -50,6 +61,28 @@ public class App {
                 CounterService counterService) {
             return new FrenglyTranslationService(restTemplate, settings, counterService);
         }
+    }
+
+    @Profile("!aws")
+    @Configuration
+    static class LocalCacheConfig {
+
+        @Bean
+        public CacheManager createSimpleCacheManager() {
+            SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
+            List<Cache> caches = new ArrayList<>();
+            caches.add(new ConcurrentMapCache("translations"));
+            simpleCacheManager.setCaches(caches);
+            return simpleCacheManager;
+        }
+
+    }
+
+    @Profile("aws")
+    @Configuration
+    @EnableElastiCache({ @CacheClusterConfig(name = "translations" /*, expiration = 1209600 */) })
+    static class ElastiCacheConfig {
+
     }
 
 
